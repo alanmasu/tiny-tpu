260076ba4bee0fb9bf002d1cb924a066a10b4c91
alanmasu <alan.masutti@gmail.com>
2025-12-09 00:31:59 +0100

    Completated PE tests - ALL GOOD

--- test/test_pe.sv
+++ test/test_pe.sv
@@ -215,7 +215,7 @@ module test_pe_tb;
         pe_weight_in = to_fixed(0.0);
         pe_valid_in = 0;
         pe_input_in = to_fixed(0);        
-        pe_switch_in = 1;
+        pe_switch_in = 0;
         #1;
         valitading = 1'b1;
         //Cheking inteternal registers
@@ -249,9 +249,47 @@ module test_pe_tb;
         #1;
         valitading = 1'b0;
        
-        
-
-
+        testN = 5;
+        @(posedge clk);
+        #1;                 
+        pe_enabled_in = 1;    
+        pe_accept_w_in = 0;
+        pe_weight_in = to_fixed(0.0);
+        pe_valid_in = 0;
+        pe_input_in = to_fixed(0);        
+        pe_switch_in = 0;
+        #1;
+        valitading = 1'b1;
+        //Cheking inteternal registers
+        if(dut.weight_reg_inactive == to_fixed(5.75)) begin
+            $display("Test #%0da OK", testN);
+        end else begin
+            $display("Test #%0da FAIL => weight_reg_inactive was %f, expected %f", testN, from_fixed(dut.weight_reg_inactive), 5.75);
+        end 
+        if(dut.weight_reg_active == to_fixed(5.75)) begin
+            $display("Test #%0db OK", testN);
+        end else begin
+            $display("Test #%0db FAIL => weight_reg_inactive was %f, expected %f", testN, from_fixed(dut.weight_reg_inactive), 5.75);
+        end 
+        //Checking south ouptut
+        if(pe_weight_out == to_fixed(0.0)) begin
+            $display("Test #%0dc OK", testN);
+        end else begin
+            $display("Test #%0dc FAIL => pe_weight_out was %f, expected %f", testN, from_fixed(pe_weight_out), 5.75);
+        end
+        if(pe_psum_out == to_fixed(111.31640625)) begin 
+            $display("Test #%0dd OK", testN);
+        end else begin
+            $display("Test #%0dd FAIL => pe_psum_out was %f, expected %f", testN, from_fixed(pe_psum_out), 111.31640625);
+        end
+        //Cheching east ouptut
+        if(pe_input_out == to_fixed(19.359375)) begin
+            $display("Test #%0de OK", testN);
+        end else begin
+            $display("Test #%0de FAIL => pe_input_out was %f, expected %f", testN, from_fixed(pe_input_out), 19.359375);
+        end
+        #1;
+        valitading = 1'b0;
 
         repeat(5) @(posedge clk);
     
