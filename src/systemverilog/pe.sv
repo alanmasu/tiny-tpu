c65ccb2577005100bdd305aaaa1e4ad277637669
alanmasu <alan.masutti@gmail.com>
2025-12-12 18:59:23 +0100

    Changed 'peswitch_in' behaviour by adding a register on it instead using it as a LE for 'w_active'
     - By now, the previous behavious is restored but in a safe manner.

--- src/systemverilog/pe.sv
+++ src/systemverilog/pe.sv
@@ -34,6 +34,7 @@ module pe #(
     wire signed [15:0] mac_out; // just a wire
     logic signed [15:0] weight_reg_active; // foreground register
     logic signed[15:0] weight_reg_inactive; // background register
+    reg pe_switch_in_reg;
 
     fxp_mul mult (
         .ina(pe_input_out),
@@ -51,13 +52,13 @@ module pe #(
 
     // Only the switch flag is combinational (active register copies inactive register on the same clock cycle that switch flag is set)
     // That means inputs from the left side of the PE can load in on the same clock cycle that the switch flag is set
-    // always_comb begin
-    //     if (rst || !pe_enabled) begin
-    //         weight_reg_active = 16'b0;
-    //     end else if (pe_switch_in) begin
-    //         weight_reg_active = weight_reg_inactive;
-    //     end
-    // end
+    always_comb begin
+        if (rst || !pe_enabled) begin
+            weight_reg_active = 16'b0;
+        end else if (pe_switch_in_reg) begin
+            weight_reg_active = weight_reg_inactive;
+        end
+    end
 
     always @(posedge clk or posedge rst) begin
         if (rst || !pe_enabled) begin
@@ -67,17 +68,19 @@ module pe #(
             pe_weight_out <= 16'b0;
             pe_switch_out <= 0;
             pe_psum_out <= 16'b0;
-            weight_reg_active = 16'b0;
+            // weight_reg_active = 16'b0;
             pe_accept_w_out <= 0;
+            pe_switch_in_reg <= 0;
         end else begin
             pe_valid_out <= pe_valid_in;
             pe_switch_out <= pe_switch_in;
             pe_psum_out <= mac_out;
             pe_accept_w_out <= pe_accept_w_in;
+            pe_switch_in_reg <= pe_switch_in;
 
-            if (pe_switch_in) begin
-                weight_reg_active <= weight_reg_inactive;
-            end
+            // if (pe_switch_in) begin
+            //     weight_reg_active <= weight_reg_inactive;
+            // end
             
             // Weight register updates - only on clock edges
             if (pe_accept_w_in) begin
